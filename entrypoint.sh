58f1689fd03b536f2afb85240d8f8f6cea88232d
fthornton67 <fthornton67@gmail.com>
2025-05-30 10:40:25 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 # Wait for SQL Server to be ready
 echo "Waiting for SQL Server to start..."
 for i in {1..60}; do
-    if /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "SELECT 1" &>/dev/null; then
+    if /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P $MSSQL_SA_PASSWORD -Q "SELECT 1" &>/dev/null; then
         echo "SQL Server is up and running"
         break
     fi
