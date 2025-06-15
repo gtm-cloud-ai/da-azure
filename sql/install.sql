970bfadb718187ba3c21d862bf5f0b2aba3d9119
Fredrick Thornton <fthornton67@gmail.com>
2025-06-15 12:50:24 +0000

    chore: update database migration script [skip ci]

--- sql/install.sql
+++ sql/install.sql
@@ -648,9 +648,9 @@ GO
 INSERT INTO ValueTypes
     (ValueType,CreateDate,HTMLType)
 VALUES 
-    ('Int','06/15/2025 12:43:00','int'),
-    ('String','06/15/2025 12:43:00','string'),
-    ('Date','06/15/2025 12:43:00','date')
+    ('Int','06/15/2025 12:50:22','int'),
+    ('String','06/15/2025 12:50:22','string'),
+    ('Date','06/15/2025 12:50:22','date')
 
 GO
 
@@ -661,9 +661,9 @@ GO
 INSERT INTO Events
 	(EventsName,IsActive,CreateDate)
 VALUES
-    ('Activate',1,'06/15/2025 12:43:00'),
-	('Unsubscribe',1,'06/15/2025 12:43:00'),
-	('Pending Activation',1,'06/15/2025 12:43:00')
+    ('Activate',1,'06/15/2025 12:50:22'),
+	('Unsubscribe',1,'06/15/2025 12:50:22'),
+	('Pending Activation',1,'06/15/2025 12:50:22')
 
 GO
 
@@ -711,7 +711,7 @@ GO
 INSERT INTO EmailTemplate
 	([Status],[Description],[InsertDate],[TemplateBody],[Subject],[IsActive])
 VALUES
-    ('Failed','Failed','06/15/2025 12:43:00', '
+    ('Failed','Failed','06/15/2025 12:50:22', '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
 <html xmlns="http://www.w3.org/1999/xhtml">
    <head>
@@ -773,7 +773,7 @@ VALUES
       </center>
    </body>
 </html>','Failed',1),
-	('PendingActivation','Pending Activation','06/15/2025 12:43:00', '
+	('PendingActivation','Pending Activation','06/15/2025 12:50:22', '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
 <html xmlns="http://www.w3.org/1999/xhtml">
    <head>
@@ -836,7 +836,7 @@ VALUES
    </body>
 </html>
 ','Pending Activation',1),
-	('Subscribed','Subscribed','06/15/2025 12:43:00', '
+	('Subscribed','Subscribed','06/15/2025 12:50:22', '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
 <html xmlns="http://www.w3.org/1999/xhtml">
    <head>
@@ -899,7 +899,7 @@ VALUES
    </body>
 </html>
 ','Subscribed',1),
-	('Unsubscribed','Unsubscribed','06/15/2025 12:43:00', '
+	('Unsubscribed','Unsubscribed','06/15/2025 12:50:22', '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
 <html xmlns="http://www.w3.org/1999/xhtml">
    <head>
